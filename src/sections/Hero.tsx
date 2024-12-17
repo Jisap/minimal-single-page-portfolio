8444fa37ba1954349ab0897097abd8f7ccd36fd4
Jorge Matos <jisapjisap@gmail.com>
2024-12-17 18:42:00 +0100

    hero

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -7,46 +7,52 @@ import Button from "@/components/Button";
 
 const Hero: FC = () => {
   return (
-    <section className="">
-      <div className="container !max-w-full">
-        <h1 className="text-5xl mt-40">
-          Crafting digital experiences through code and creative desing
-        </h1>
-        <div className="flex flex-col mt-20 items-start gap-6">
-          <Button 
-            variant="secondary"
-            iconAfter={
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="size-5"
+    <section>
+      <div className="grid md:grid-cols-12 md:h-screen items-stretch">
+        <div className="md:col-span-7 flex flex-col justify-center">
+          <div className="container !max-w-full">
+            <h1 className="text-5xl md:text-6xl mt-40 md:mt-0">
+              Crafting digital experiences through code and creative desing
+            </h1>
+            <div className="flex flex-col mt-20 items-start gap-6">
+              <Button 
+                variant="secondary"
+                iconAfter={
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="size-5"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"
+                    />
+                  </svg>
+                }  
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"
-                />
-              </svg>
-            }  
-          >
-            <span>
-              View My Work
-            </span>
-          </Button>
-          <Button variant="text">
-            Let&apos;s Talk
-          </Button>
+                <span>
+                  View My Work
+                </span>
+              </Button>
+              <Button variant="text">
+                Let&apos;s Talk
+              </Button>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="mt-20">
-        <Image 
-          src={heroImage}
-          alt="Hero Image"
-
-        />
+        <div className="md:col-span-5">
+          <div className="mt-20 md:mt-0 h-full">
+            <Image 
+              src={heroImage}
+              alt="Hero Image"
+              className="size-full object-cover"
+            />
+          </div>
+        </div>
       </div>
     </section>
   )
