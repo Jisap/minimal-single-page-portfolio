d01d13d2930aa028771ea1c840810faca221e10c
Jorge Matos <jisapjisap@gmail.com>
2025-01-09 13:24:53 +0100

    Footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,7 +1,53 @@
 import { FC } from "react";
 
+
+const navItems = [
+  {
+    href: "#",
+    label: "Home"
+  },
+  {
+    href: "#",
+    label: "Projects"
+  },
+  {
+    href: "#",
+    label: "Testimonials"
+  },
+  {
+    href: "#",
+    label: "Faqs"
+  },
+  {
+    href: "#",
+    label: "Contact"
+  },
+]
+
 const Footer: FC = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className="container">
+        <div>
+          One spot available for next month
+        </div>
+        <h2>
+          Enough talk. Let&apos;s make something great together.
+        </h2>
+        <button>info@alextaylor.com</button>
+        <nav>
+          {navItems.map(({ href, label }) => (
+            <a href={href} key={label}>
+              {label}
+            </a>
+          ))} 
+        </nav>
+        <p>
+          Copyright &copy; 2025 Alex Taylor. &bull; All rights reserved.
+        </p>
+      </div>
+    </footer>
+  ) 
 };
 
 export default Footer;
