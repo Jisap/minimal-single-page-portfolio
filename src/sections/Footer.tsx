64361bfeaf4ab0c4512b9cd336708783f560df49
Jorge Matos <jisapjisap@gmail.com>
2025-01-09 14:32:44 +0100

    Footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import Button from "@/components/Button";
 
 
 const navItems = [
@@ -26,23 +27,56 @@ const navItems = [
 
 const Footer: FC = () => {
   return (
-    <footer>
+    <footer className="bg-stone-900 text-white">
       <div className="container">
-        <div>
-          One spot available for next month
+        <div className="section">
+          <div className="flex items-center gap-3">
+            <div className="size-3 rounded-full bg-green-400"></div>
+            <span className="uppercase">
+              One spot available for next month
+            </span>
+          </div>
+
+          <h2 className="text-4xl mt-8 font-extralight">
+            Enough talk. Let&apos;s make something great together.
+          </h2>
+
+          <Button
+            variant="secondary"
+            className="mt-8"
+            iconAfter={
+              <svg 
+                xmlns="http://www.w3.org/2000/svg" 
+                fill="none" 
+                viewBox="0 0 24 24" 
+                strokeWidth="1.5" 
+                stroke="currentColor" 
+                className="size-6"
+              >
+                <path 
+                  strokeLinecap="round" 
+                  strokeLinejoin="round" 
+                  d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" 
+                />
+              </svg> 
+            }>
+            info@alextaylor.com
+          </Button>
+
+          <nav className="flex flex-col gap-8 mt-16">
+            {navItems.map(({ href, label }) => (
+              <a href={href} key={label}>
+                <Button 
+                  variant="text"
+                  className="text-lg"  
+                >
+                  {label}
+                </Button>
+              </a>
+            ))} 
+          </nav>
         </div>
-        <h2>
-          Enough talk. Let&apos;s make something great together.
-        </h2>
-        <button>info@alextaylor.com</button>
-        <nav>
-          {navItems.map(({ href, label }) => (
-            <a href={href} key={label}>
-              {label}
-            </a>
-          ))} 
-        </nav>
-        <p>
+        <p className="py-16 text-white/30 text-sm">
           Copyright &copy; 2025 Alex Taylor. &bull; All rights reserved.
         </p>
       </div>
