e516966b69e05b12402c26753faf02bb881342a9
Jorge Matos <jisapjisap@gmail.com>
2025-01-10 19:14:35 +0100

    Add animation to Header component toggle functionality

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,5 +1,8 @@
+"use client"
+
+import { FC, useEffect, useState } from "react";
 import Button from "@/components/Button";
-import { FC } from "react";
+import { motion, useAnimate } from "motion/react";
 
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 const navItems = [
@@ -26,6 +29,81 @@ const navItems = [
 ];
 
 const Header: FC = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [topLineScope, topLineAnimate] = useAnimate();        // animación para la linea superior
+  const [bottomLineScope, bottomLineAnimate] = useAnimate();  // animación para la linea inferior
+
+  useEffect(() => {
+    if(isOpen){
+      topLineAnimate([
+        [
+          topLineScope.current,
+          {
+            translateY: 4
+          }
+        ],
+        [
+          topLineScope.current,
+          {
+            rotate: 45
+          }
+        ]
+      ]);
+
+      bottomLineAnimate([
+        [
+          bottomLineScope.current,
+          {
+            translateY: -4
+          }
+        ],
+        [
+          bottomLineScope.current,
+          {
+            rotate: -45
+          }
+        ]
+      ]);
+    }else{
+      topLineAnimate([
+        [
+          topLineScope.current,
+          {
+            rotate: 0
+          }
+        ],
+        [
+          topLineScope.current,
+          {
+            translateY: 0
+          }
+        ]
+      ]);
+
+      bottomLineAnimate([
+        [
+          bottomLineScope.current,
+          {
+            rotate: 0
+          }
+        ],
+        [
+          bottomLineScope.current,
+          {
+            translateY: 0
+          }
+        ]
+      ])
+    }
+  },[
+    isOpen,
+    topLineScope,
+    bottomLineScope,
+    topLineAnimate,
+    bottomLineAnimate
+  ])
+
   return (
     <header>
       <div className="fixed top-0 left-0 w-full mix-blend-difference backdrop-blur-md">
@@ -44,21 +122,47 @@ const Header: FC = () => {
 
       <div className="fixed top-0 left-0 w-full ">
         <div className="container !max-w-full">
-          <div className="flex justify-end h-20 items-center"> 
+          <div className="flex justify-end h-20 items-center">
             <div className="flex items-center gap-4">
-              <div className="size-11 border border-stone-400 rounded-full inline-flex items-center justify-center bg-stone-200">
-                <svg 
-                  width="24" 
-                  height="24" 
-                  viewBox="0 0 24 24" 
-                  fill="none" 
+              <div 
+                onClick={() => setIsOpen(!isOpen)}
+                className="size-11 border border-stone-400 rounded-full inline-flex items-center justify-center bg-stone-200"
+              >
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
                   xmlns="http://www.w3.org/2000/svg"
                 >
-                  <rect x="3" y="15" width="18" height="2" fill="currentColor" />
-                  <rect x="3" y="7" width="18" height="2" fill="currentColor" />
+                  <motion.rect
+                    x="3"
+                    y="7"
+                    width="18"
+                    height="2"
+                    fill="currentColor"
+                    ref={topLineScope}
+                    style={{
+                      transformOrigin: "12px 8px",
+                      //transform: "translateY(4px) rotate(45deg)"
+                    }}
+                  />
+                  <motion.rect 
+                    x="3" 
+                    y="15" 
+                    width="18" 
+                    height="2" 
+                    fill="currentColor"
+                    ref={bottomLineScope} 
+                    style={{
+                      transformOrigin: "12px 16px",
+                      //transform: "translateY(-4px) rotate(-45deg)"
+                    }}
+                  />
+                  
                 </svg>
               </div>
-              <Button 
+              <Button
                 className="hidden md:inline-flex"
                 variant="primary"
               >
@@ -72,4 +176,4 @@ const Header: FC = () => {
     </header>
   )
 }
-  export default Header
+export default Header
