b3a8ce955050ed24926e1dc693edc16a1cb9b865
Jorge Matos <jisapjisap@gmail.com>
2025-01-15 16:26:42 +0100

    Enhance Button and Hero components with improved hover effects and animations

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -54,20 +54,38 @@ const Hero: FC = () => {
                 <Button 
                   variant="secondary"
                   iconAfter={
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      className="size-5"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"
-                      />
-                    </svg>
+                    <div className="overflow-hidden size-5">
+                      <div className="h-5 w-10 flex group-hover/button:-translate-x-1/2 transition-transform duration-500">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth="1.5"
+                          stroke="currentColor"
+                          className="size-5"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"
+                          />
+                        </svg>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth="1.5"
+                          stroke="currentColor"
+                          className="size-5"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"
+                          />
+                        </svg>
+                      </div>
+                    </div>
                   }  
                 >
                   <span>
